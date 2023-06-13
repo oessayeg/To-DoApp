8bdb72f626190f6e3f2e920190d50bf532a115a1
Othmane <offissayegh001@gmail.com>
2023-06-13 12:34:26 +0100

    Add transition to hide-nav button

--- src/modules/hideNav.js
+++ src/modules/hideNav.js
@@ -8,11 +8,15 @@ export default function hideNavigation(button)
 		if (button.className)
 		{
 			navigation.style.display = "flex";
+			button.style.transform = "rotate(0deg)";
+			button.style.transition = "transform 700ms";
 			main.style.gridTemplateColumns = "auto 1fr";
 			button.className = "";
 		}
 		else
 		{
+			button.style.transform = "rotate(180deg)";
+			button.style.transition = "transform 700ms";
 			main.style.gridTemplateColumns = "1fr";
 			navigation.style.display = "none";
 			button.className = "hidden";
